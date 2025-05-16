df68871d5f442cf9548fff147b418387c0ba30d0
Sawyer Ball <sawyer@wilcoxfresh.com>
2025-05-16 11:50:36 -0600

    Get rid of unneeded csharp lsp stuff

--- init.lua
+++ init.lua
@@ -379,6 +379,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      -- vim.keymap.set('n', '<C>V', blockwise - visual, { desc = 'Enter visual block mode' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
